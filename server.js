1bcdb5fb7400d68b13e1f4f1486277cf50127804
Rick Brown <ricky.brown.00@gmail.com>
2020-05-01 23:56:25 +0100

    adds server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cookie = require('cookie-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(cookie())
+app.use(express.json())
+
+mongoose.connect(
+  'mongodb://localhost:27017/jwt-auth',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log('📦 => connected to the database')
+  }
+)
+
+app.listen(5000, () => {
+  console.log(`🌎 => connected to http server`)
+})
